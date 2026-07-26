6d7d10a91aeaf8a9021eb20af6cc5e818fb28146
Jashveer Lamba <nightnine91@gmail.com>
2026-07-26 12:49:31 +0530

    update

--- receiver/include/UIManager.h
+++ receiver/include/UIManager.h
@@ -15,12 +15,18 @@ class UIManager
 {
 public:
     UIManager() = default;
-    ~UIManager() = default;
+    ~UIManager();
 
     bool Create(HWND parent);
     void UpdateLayout();
 
     void SetConnectionStatus(ConnectionStatus status);
+
+    HWND GetContentPanelHandle() const { return m_contentPanel; }
+
+private:
+    static ATOM RegisterContentPanelClass(HINSTANCE instance);
+    static LRESULT CALLBACK ContentPanelProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 private:
     HWND m_parent = nullptr;
@@ -29,6 +35,9 @@ private:
     HWND m_logoLabel = nullptr;
     HWND m_statusLabel = nullptr;
     ToggleSwitch m_connectToggle;
+
+    // Main Content Panel
+    HWND m_contentPanel = nullptr;
 
     ConnectionStatus m_currentStatus = ConnectionStatus::Disconnected;
 
@@ -41,4 +50,7 @@ private:
     static constexpr int STATUS_HEIGHT = 25;
     static constexpr int TOGGLE_WIDTH = 70;
     static constexpr int TOGGLE_HEIGHT = 35;
+    static constexpr int TOP_BAR_TOTAL_HEIGHT = 60;
+
+    static constexpr const wchar_t* CONTENT_PANEL_CLASS = L"SYNKROAD_ContentPanel";
 };
--- receiver/src/UIManager.cpp
+++ receiver/src/UIManager.cpp
@@ -1,8 +1,68 @@
 #include "UIManager.h"
 
+UIManager::~UIManager() = default;
+
+ATOM UIManager::RegisterContentPanelClass(HINSTANCE instance)
+{
+    static bool registered = false;
+    if (registered) return 0;
+
+    WNDCLASSEXW wc = { sizeof(WNDCLASSEXW) };
+    wc.style = CS_HREDRAW | CS_VREDRAW;
+    wc.lpfnWndProc = UIManager::ContentPanelProc;
+    wc.cbClsExtra = 0;
+    wc.cbWndExtra = 0;
+    wc.hInstance = instance;
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+    // Distinct soft gray background brush for content panel
+    wc.hbrBackground = CreateSolidBrush(RGB(245, 246, 248));
+    wc.lpszClassName = CONTENT_PANEL_CLASS;
+
+    registered = true;
+    return RegisterClassExW(&wc);
+}
+
+LRESULT CALLBACK UIManager::ContentPanelProc(HWND hwnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_ERASEBKGND:
+        return 1; // Handled by class background brush
+
+    case WM_PAINT:
+    {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hwnd, &ps);
+        
+        RECT rc;
+        GetClientRect(hwnd, &rc);
+        HBRUSH bgBrush = CreateSolidBrush(RGB(245, 246, 248));
+        FillRect(hdc, &rc, bgBrush);
+        DeleteObject(bgBrush);
+
+        // Draw top separator border line between header bar and content area
+        HPEN borderPen = CreatePen(PS_SOLID, 1, RGB(225, 228, 232));
+        HPEN oldPen = static_cast<HPEN>(SelectObject(hdc, borderPen));
+        MoveToEx(hdc, 0, 0, nullptr);
+        LineTo(hdc, rc.right, 0);
+        SelectObject(hdc, oldPen);
+        DeleteObject(borderPen);
+
+        EndPaint(hwnd, &ps);
+        return 0;
+    }
+    }
+
+    return DefWindowProcW(hwnd, message, wParam, lParam);
+}
+
 bool UIManager::Create(HWND parent)
 {
     m_parent = parent;
+    HINSTANCE instance = GetModuleHandleW(nullptr);
+
+    // Register Content Panel Class
+    RegisterContentPanelClass(instance);
 
     // SYNKROAD Logo Label
     m_logoLabel = CreateWindowExW(
@@ -13,7 +73,7 @@ bool UIManager::Create(HWND parent)
         0, 0, 0, 0,
         m_parent,
         nullptr,
-        GetModuleHandleW(nullptr),
+        instance,
         nullptr);
 
     if (!m_logoLabel)
@@ -28,7 +88,7 @@ bool UIManager::Create(HWND parent)
         0, 0, 0, 0,
         m_parent,
         nullptr,
-        GetModuleHandleW(nullptr),
+        instance,
         nullptr);
 
     if (!m_statusLabel)
@@ -45,12 +105,26 @@ bool UIManager::Create(HWND parent)
 
     m_connectToggle.SetChecked(false);
 
-    // Wire up toggle switch to demo status changes
     m_connectToggle.SetOnToggle(
         [this](bool connected)
         {
             SetConnectionStatus(connected ? ConnectionStatus::Connected : ConnectionStatus::Disconnected);
         });
+
+    // Content Panel Window Container
+    m_contentPanel = CreateWindowExW(
+        0,
+        CONTENT_PANEL_CLASS,
+        L"",
+        WS_CHILD | WS_VISIBLE | WS_CLIPCHILDREN,
+        0, 0, 0, 0,
+        m_parent,
+        nullptr,
+        instance,
+        nullptr);
+
+    if (!m_contentPanel)
+        return false;
 
     // Initial Status & Layout Calculation
     SetConnectionStatus(ConnectionStatus::Disconnected);
@@ -91,6 +165,7 @@ void UIManager::UpdateLayout()
     GetClientRect(m_parent, &clientRect);
 
     int clientWidth = clientRect.right - clientRect.left;
+    int clientHeight = clientRect.bottom - clientRect.top;
 
     // 1. Position SYNKROAD Logo (Left-aligned)
     if (m_logoLabel)
@@ -130,6 +205,23 @@ void UIManager::UpdateLayout()
             TOP_BAR_PADDING_Y + 3,
             STATUS_WIDTH,
             STATUS_HEIGHT,
+            SWP_NOZORDER | SWP_NOACTIVATE);
+    }
+
+    // 4. Position Content Panel (Fills full remaining client area beneath top bar)
+    if (m_contentPanel)
+    {
+        int contentY = TOP_BAR_TOTAL_HEIGHT;
+        int contentHeight = clientHeight - contentY;
+        if (contentHeight < 0) contentHeight = 0;
+
+        SetWindowPos(
+            m_contentPanel,
+            nullptr,
+            0,
+            contentY,
+            clientWidth,
+            contentHeight,
             SWP_NOZORDER | SWP_NOACTIVATE);
     }
 }
